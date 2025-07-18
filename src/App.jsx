1837bb471af11411aff9ba9bf679e19cf6d480cb
Renz Angelo T. Sefuentes <142190484+SefuentesRenz@users.noreply.github.com>
2025-07-19 06:54:39 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,12 @@ import StudentPage from './pages/StudentPage';
 import ChooseCategory from './pages/ChooseCategory';
 import ChooseDifficulty from './pages/ChooseDifficulty';
 import StudentProfile from './pages/StudentProfile';
+
+//Admin pages
+import ActivitiesPage from './Admin/Activities';
+import AddActivity from './Admin/AddActivity';
+import ExpressionWall from './Admin/ExpressionWall';
+import Tracking from './Admin/Tracking';
  
 
 function App() {
@@ -22,6 +28,13 @@ function App() {
       <Route path="/choosecategory" element={<ChooseCategory />} />
       <Route path="/choosedifficulty" element={<ChooseDifficulty />} />
       <Route path="/studentprofile" element={<StudentProfile />} />
+      
+
+      {/* Admin Pages */}
+      <Route path="/activities" element={<ActivitiesPage />} />
+      <Route path="/addactivity" element={<AddActivity />} />
+      <Route path="/expressionwall" element={<ExpressionWall />} />
+      <Route path="/tracking" element={<Tracking />} />
       
     </Routes>
   );
